f2dbdb988be335982a80f1c2afd762e3cc2187dd
Arthur Xavier <arthurcsxavier@gmail.com>
2022-07-05 17:52:55 -0300

    Rmv do node_modules

--- dist/database/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const pg_1 = require("pg");
-const connectionString = `postgres://${process.env.DB_User}:${process.env.DB_Password}@motty.db.elephantsql.com/dxhkhrst`;
-const db = new pg_1.Pool({ connectionString });
-exports.default = db;
--- dist/routes/statusRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const statusRoute = (0, express_1.Router)();
-statusRoute.get('/', (req, res) => {
-    res.status(200).json({ message: 'Bem-vindo a Biblioteca Poli Junior!' });
-});
-exports.default = statusRoute;
